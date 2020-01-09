32b70cb7f1c65f2b0e7f2f196b51e73f632655ea
hjsbs <59188658+hjsbs@users.noreply.github.com>
2020-01-09 21:17:43 +0800

    Update 1.js

--- js/1.js
+++ js/1.js
@@ -4,7 +4,7 @@ if (document.getElementById('evanyou')) {
     pr = window.devicePixelRatio || 1,
     w = window.innerWidth,
     h = window.innerHeight,
-    f = 50,
+    f = 300,
     q,
     m = Math,
     r = 0,
