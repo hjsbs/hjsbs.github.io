6b26261a692a9c6300521109f1e4c07e53017947
hjsbs <59188658+hjsbs@users.noreply.github.com>
2020-01-13 14:44:32 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,3 @@
-// sw.js 文件
- 
-// 安装
-self.addEventListener('install', function (e) {
-    // 缓存 App Shell 等关键静态资源和 html (保证能缓存的内容能在离线状态跑起来)
-});
- 
-// 激活
-self.addEventListener('activate', function (e) {
-    // 激活的状态，这里就做一做老的缓存的清理工作
-});
- 
-// 缓存请求和返回（这是个简单的缓存优先的例子）
-self.addEventListener('fetch', function (e) {
-    e.respondWith(caches.match(e.request)
-        .then(function (response) {
-            if (response) {
-                return response;
-            }
-            // fetchAndCache 方法并不存在，需要自己定义，这里只是示意代码
-            return fetchAndCache(e.request);
-        })
-    );
-});
+self.addEventListener('fetch', function (evt) {
+    console.log('sw fetch() 发送的请求', evt.request.url)
+})
